9960fbd8653eb582d0a3a55474fa6d675918822d
Debashis Roy <goodquote39@hotmail.com>
2023-06-05 01:48:33 +0600

    project started again

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h2>R-SHOP</h2>
+        <p>tailwind </p>
+        <p>firebase</p>
+        <p>dynamic navbar </p>
+        <p>react router</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
