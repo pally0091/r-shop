3f78f0b4cb937e30f2b7a6d54882c5cc478e65dd
Debashis Roy <goodquote39@hotmail.com>
2023-06-09 08:26:05 +0600

    logo added

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -1,14 +1,23 @@
 /* eslint-disable no-unused-vars */
 import React from "react";
 import Nav from "./Nav";
-import { Outlet } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
 import SideNav from "./SideNav";
+import logo from "../assets/Logo.png";
 
 const Main = () => {
   return (
     <div>
       <div className="bg-red-400 p-5">
-        <div>LOGO</div>
+        <div className="w-32 mx-auto ring-2 p-1 rounded-full">
+          <Link to="/">
+            <img
+              className="rounded-full"
+              src={logo}
+              alt=""
+            />
+          </Link>
+        </div>
       </div>
       <div className="flex">
         <div className="w-[20%]">
--- src/Components/Signup.jsx
+++ src/Components/Signup.jsx
@@ -1,11 +1,21 @@
 /* eslint-disable no-unused-vars */
 import React from "react";
+import { Link } from "react-router-dom";
+import logo from "../assets/Logo.png";
 
 const Signup = () => {
   return (
     <div>
       <div className="bg-red-400 p-5">
-        <div>LOGO</div>
+        <div className="w-32 mx-auto ring-2 p-1 rounded-full">
+          <Link to="/">
+            <img
+              className="rounded-full"
+              src={logo}
+              alt=""
+            />
+          </Link>
+        </div>
       </div>
       <h1>register</h1>
     </div>
