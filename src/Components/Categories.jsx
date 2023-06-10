9d046239807df2b9ebdf3f5c9864dfbba37775f0
Debashis Roy <goodquote39@hotmail.com>
2023-06-11 02:00:33 +0600

    spinner added

--- src/Components/Categories.jsx
+++ src/Components/Categories.jsx
@@ -2,26 +2,43 @@
 /* eslint-disable no-unused-vars */
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import { SyncLoader } from "react-spinners";
 
 const Categories = () => {
   const [categories, setCategories] = useState();
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     fetch("https://dummyjson.com/products/categories")
       .then((response) => response.json())
       .then((data) => setCategories(data));
-
     // console.log(categories);
   }, []);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+  }, []);
   return (
-    <div className="overflow-y-scroll h-96">
-      {categories?.map((category) => (
-        <Link to={`/category/${category}`}>
-          <button className="w-full my-1 bg-cyan-200 py-1 uppercase hover:bg-sky-700 hover:text-white transition-all duration-700">
-            {category}
-          </button>
-        </Link>
-      ))}
+    <div className="flex justify-center">
+      {loading ? (
+        <SyncLoader
+          color="#eb8a8f"
+          margin={5}
+          size={25}
+        />
+      ) : (
+        <div className="overflow-y-scroll h-96">
+          {categories?.map((category) => (
+            <Link to={`/category/${category}`}>
+              <button className="w-full my-1 bg-cyan-200 py-1 uppercase hover:bg-sky-700 hover:text-white transition-all duration-700">
+                {category}
+              </button>
+            </Link>
+          ))}
+        </div>
+      )}
     </div>
   );
 };
--- src/Components/Products.jsx
+++ src/Components/Products.jsx
@@ -1,22 +1,38 @@
 /* eslint-disable no-undef */
 /* eslint-disable no-unused-vars */
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useLoaderData } from "react-router-dom";
 import Product from "./Product";
+import { PuffLoader } from "react-spinners";
 
 const Products = () => {
   const data = useLoaderData();
   const products = data.products;
 
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+  }, []);
   // console.log(products);
   return (
-    <div className="grid grid-cols-3 gap-3 w-[98%] mt-4 mx-auto">
-      {products?.map((product) => (
-        <Product
-          key={product.id}
-          product={product}
-        ></Product>
-      ))}
+    <div className="flex justify-center">
+      {loading ? (
+        <PuffLoader
+          color="#eb8a8f"
+          size={150}
+        />
+      ) : (
+        <div className="grid grid-cols-3 gap-3 w-[98%] mt-4 mx-auto">
+          {products?.map((product) => (
+            <Product
+              key={product.id}
+              product={product}
+            ></Product>
+          ))}
+        </div>
+      )}
     </div>
   );
 };
