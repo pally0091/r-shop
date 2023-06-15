e7d41c798c4ee322eb7194033768e741bc41b0e1
Debashis Roy <goodquote39@hotmail.com>
2023-06-15 08:56:46 +0600

    home page banner

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,12 +1,14 @@
 /* eslint-disable no-unused-vars */
 import React from "react";
 import HotSaleContainer from "./HotSaleContainer";
+import Banner from "./Banner";
 
 const Home = () => {
   return (
     <div>
       <div>
         <HotSaleContainer></HotSaleContainer>
+        <Banner></Banner>
       </div>
     </div>
   );
