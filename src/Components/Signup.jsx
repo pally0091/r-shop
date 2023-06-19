e38befc5c0b5c22ce6fcf43f6102c4d36bc27b1c
Debashis Roy <goodquote39@hotmail.com>
2023-06-19 10:48:17 +0600

    registration with no error

--- src/Components/Signup.jsx
+++ src/Components/Signup.jsx
@@ -3,6 +3,7 @@ import React, { useContext, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import logo from "../assets/Logo.png";
 import { AuthContext } from "./Context";
+import { updateProfile } from "firebase/auth";
 
 const Signup = () => {
   const { createUser, loading, setLoading } = useContext(AuthContext);
